4b03d69fb698ef3ed96f7e548f9aba57d5f36ed4
23Aryansharma11 <23aryansharma11@gmail.com>
2025-05-03 10:43:51 +0530

    fix spelling error

--- src/components/MeetTheTeam.tsx
+++ src/components/MeetTheTeam.tsx
@@ -42,7 +42,7 @@ export function MeetTheTeam() {
     return (
         <DraggableCardContainer className="relative flex min-h-screen w-full items-center justify-center overflow-clip">
             <p className="absolute top-1/2 mx-auto max-w-sm -translate-y-3/4 text-center text-2xl font-black text-neutral-400 md:text-4xl dark:text-neutral-800">
-                AI Tounge Insight's Team
+                AI Tongue Insight's Team
             </p>
             {items.map((item) => (
                 <DraggableCardBody className={item.className} key={item.title}>
